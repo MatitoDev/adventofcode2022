1acb6569b8aafa699d10992b9ea326a693a0d1ef
Matito <matitos@outlook.de>
2022-12-02 15:02:17 +0100

    part2

--- day2.py
+++ day2.py
@@ -3,18 +3,39 @@ f = open("day2_input.txt", "r").read().splitlines()
 points = 0
 
 for i in range(len(f)):
-    
-    round = f[i].split(" ")
 
+    round = f[i].split(" ")
+#X - lose
+#Y - draw
+#Z - win
     if round[1] == 'X':
-        round[1] = 'A'
-        points = points + 1
+        if round[0] == 'A':
+            round[1] = 'C'
+            points = points + 3
+        elif round[0] == 'B':
+            round[1] = 'A'
+            points = points + 1
+        elif round[0] == 'C':
+            round[1] = 'B'
+            points = points + 2
     elif round[1] == 'Y':
-        round[1] = 'B'
-        points = points + 2
+        round[1] = round[0]
+        if round[1] == 'A':
+            points = points + 1
+        elif round[1] == 'B':
+                points = points + 2
+        elif round[1] == 'C':
+                points = points + 3
     elif round[1] == 'Z':
-        round[1] = 'C'
-        points = points + 3
+        if round[0] == 'A':
+            round[1] = 'B'
+            points = points + 2
+        elif round[0] == 'B':
+            round[1] = 'C'
+            points = points + 3
+        elif round[0] == 'C':
+            round[1] = 'A'
+            points = points + 1
 
 
     if round[0] == round[1]:
@@ -24,7 +45,7 @@ for i in range(len(f)):
     elif round[0] == 'B' and round[1] == 'C':
         points = points + 6
     elif round[0] == 'C' and round[1] == 'A':
-        points = points +6
+        points = points + 6
 
 
 print(points)
