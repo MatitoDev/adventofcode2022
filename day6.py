bb1fc0dd61e4cc7346ed3ef4c05f2b1a0752cb3c
Matito <matitos@outlook.de>
2022-12-11 00:57:11 +0100

    typo

--- day6.py
+++ day6.py
@@ -3,7 +3,7 @@ f = open("day6_input.txt").read().strip()
 sop = []
 som = []
 for i in range(len(f)):
-    if len(set(f[i:i + 4])) == 4: #set -> mutable chars added only once,
+    if len(set(f[i:i + 4])) == 4: #set -> multiple chars added only once,
                                   # i:i+4 -> e.g.: 2:6 -> char 3 - 6 appears because 2:
                                   # remove first 3 chars and :6 let only first 6 appears
                                   #s/o to RainbowDashLabs from GitHub
